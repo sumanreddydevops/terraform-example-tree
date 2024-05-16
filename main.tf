5d52aa23828017d1299d82cfc87b5f5ab0858d7e
sumanreddydevops <sumanreddydevops@gmail.com>
2024-05-16 08:30:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "webserver-sg" {
+  name = "demo-sgi"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
