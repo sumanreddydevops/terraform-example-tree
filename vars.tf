a753d3bd9011f054b500a2642613c839a7aad9b5
sumanreddydevops <sumanreddydevops@gmail.com>
2024-05-16 08:33:08 +0530

    Create  vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+        default = "ami-064ff912f78e3e561"
+}
+variable "access_key" {
+        default = "AKIATCKAQOKXEATYU" // secret key not allowing to commit
+}
+variable "secret_key" {
+        default = "oz4BdPeDBB15Ift2N1gWV8TgRw0iymkJtqv"  
+}
+variable "instance_type" {
+        default = "t2.micro"
+}
+variable "region" {
+        default = "us-east-2"
+}
